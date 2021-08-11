1099be8400fca8aaad2fd21f639d591c500ef8fd
GuilhermeFA00 <sharkdevv@gmail.com>
2021-08-11 14:24:21 -0300

    upd

--- tESts.py
+++ tESts.py
@@ -1,10 +1,6 @@
 #Importando módulo como 'tt' por questões usuabilidade e velocidade
 import turtle as tt
 
-# Criando a janela
-window = tt.Screen()
-# A tartaruga
-my_turtle = tt.Turtle()
 
 #Laço principal
 while True:
@@ -14,14 +10,24 @@ while True:
                          "Se não, digite 'Exit'\n"
                          "Se sim, digite 'Tartaruga': ").capitalize()
     if user_decisao == "Tartaruga":
-        esc_primitiva = input("Escolha uma primitiva para comerçarmos o desenho e para parar digite 'Bye'\n"
-                              "Para escolher o retângulo digite '1'\n"
-                              "Para escolher o poligono regular digite '2'\n"
-                              "Para escolher o quadrado digite '3'\n"
-                              "Para escolher o triângulo retângulo digite '4'\n").capitalize()
-        while esc_primitiva == "1" or esc_primitiva == "2" or esc_primitiva == "3" or esc_primitiva == "4":
+        while True:
+            esc_primitiva = input("Escolha uma primitiva para comerçarmos o desenho e para parar digite 'Bye'\n"
+                                  "Para escolher o retângulo digite '1'\n"
+                                  "Para escolher o poligono regular digite '2'\n"
+                                  "Para escolher o quadrado digite '3'\n"
+                                  "Para escolher o triângulo retângulo digite '4'\n").capitalize()
             if esc_primitiva == "1":
-                dados_shape = input("Me diga o shape da caneta: ").lower()
+                # Criando a janela
+                window = tt.Screen()
+                # A tartaruga
+                my_turtle = tt.Turtle()
+
+                while True:
+                    dados_shape = input("Me diga o shape da caneta: ").lower()
+                    if dados_shape != "arrow" and dados_shape != "circle" and dados_shape != "classic" and dados_shape != "turtle" and dados_shape != "triangle" and dados_shape != "square":
+                        print("Escolha um modo existente no módulo")
+                    else:
+                        break
                 caneta = my_turtle.shape(dados_shape)
 
                 dados_posX = float(input("Me diga a posição x da caneta: "))
@@ -60,6 +66,11 @@ while True:
                 retangulo(Dados, Largura, Altura)
 
             if esc_primitiva == "2":
+                # Criando a janela
+                window = tt.Screen()
+                # A tartaruga
+                my_turtle = tt.Turtle()
+
                 dados_shape = input("Me diga o shape da caneta: ").lower()
                 caneta = my_turtle.shape(dados_shape)
 
@@ -87,6 +98,11 @@ while True:
                 poligonoReg(Dados, tam_lado, num_lados )
 
             if esc_primitiva == "3":
+                # Criando a janela
+                window = tt.Screen()
+                # A tartaruga
+                my_turtle = tt.Turtle()
+
                 dados_shape = input("Me diga o shape da caneta: ").lower()
                 caneta = my_turtle.shape(dados_shape)
 
@@ -114,6 +130,11 @@ while True:
                 circulo(Dados, valor_raio)
 
             if esc_primitiva == "4":
+                # Criando a janela
+                window = tt.Screen()
+                # A tartaruga
+                my_turtle = tt.Turtle()
+
                 dados_shape = input("Me diga o shape da caneta: ").lower()
                 caneta = my_turtle.shape(dados_shape)
 
@@ -151,6 +172,12 @@ while True:
                     else:
                         print("Bicho, tu estudou matemática em marte? Hipotesuna > qualquer cateto")
                 trianguloRet(Dados, valor_ctt1, valor_ctt2)
+
+            if esc_primitiva == "Bye":
+                print("Voltando ao menu")
+                break
+            else:
+                print("Por favor user, siga as regras...")
 
     if user_decisao == "Exit":
         print("Valeuuu")
